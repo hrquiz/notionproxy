794d3afb26fd6c386621485aac54ab8673803034
HR <89749037+hrquiz@users.noreply.github.com>
2021-10-13 03:46:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,16 +15,23 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/pages/6ca2dd5e-2214-4873-8ffa-d634d8ebbb53',
+        //destination: '/pages/6ca2dd5e-2214-4873-8ffa-d634d8ebbb53',
+        destination: '/pages/4f8e07bed170475195d669adae16f81c',
+
       },
     ];
   },
   env: {},
   publicRuntimeConfig: {
-    rootNotionPageId: '6ca2dd5e-2214-4873-8ffa-d634d8ebbb53',
-    rootNotionSpaceId: 'f2b37586-33c0-4236-bab4-a87c12f0d6e9',
-    gaTraceId: 'G-HDWXJVK15N',
+    //rootNotionPageId: '6ca2dd5e-2214-4873-8ffa-d634d8ebbb53',
+    //rootNotionSpaceId: 'f2b37586-33c0-4236-bab4-a87c12f0d6e9',
+    //gaTraceId: 'G-HDWXJVK15N',
+    //pageIdsThatHaveThumnail,
+    rootNotionPageId: '4f8e07bed170475195d669adae16f81c',
+    //rootNotionSpaceId: 'f2b37586-33c0-4236-bab4-a87c12f0d6e9',
+    //gaTraceId: 'G-HDWXJVK15N',
     pageIdsThatHaveThumnail,
+
   },
 };
 
